464e392815a6b4b9d83e402873839d62bcc29507
David Ordoñez <davidordonezescacena@hotmail.es>
2021-07-25 12:49:17 +0200

    detail css

--- client/src/components/CampDetail/CampDetail.js
+++ client/src/components/CampDetail/CampDetail.js
@@ -40,15 +40,18 @@ const CampDetail = ({ history, ...props }) => {
       <div>
         <Loader hidden={!loading} />
         <h1 className={'title'}>Detail Camp</h1>
-        {/* Al intentar renderizarlo me sale indefinido y da error */}
+        {/* TODO: que cambie la imagen de fondo como en la pagina inicial */}
         <section className={'container-detail'}>
           <div className={'container-title'}>
-            <h2 className={'title-camp'}>{camp.name}</h2>
+            <h2 className={'description'}>Name and direction</h2>
+            <h3 className={'title-camp'}>{camp.name}</h3>
             <p className={'address'}>{camp.address}</p>
             <p className={'location'}>{camp.location}</p>
+            <p>{camp.tag}</p>
             <p className={'edition'}>{camp.edition}</p>
           </div>
           <div className={'container-description'}>
+            <h2 className={'description'}>Description</h2>
             <p className={'message'}>{camp.description}</p>
             {camp.activities.map((activity, index) => (
               <li className={'activities'} key={index}>
@@ -57,11 +60,13 @@ const CampDetail = ({ history, ...props }) => {
             ))}
           </div>
           <div className={'container-date'}>
+            <h2 className={'description'}>Dates</h2>
             <time>{camp.createdAt}</time>
             <time>{camp.from}</time>
             <time>{camp.to}</time>
           </div>
           <div className={'container-contact'}>
+            <h2 className={'description'}>Contact</h2>
             <p>{camp.email}</p>
             <p>{camp.phone}</p>
             <p>{camp.helpers}</p>
