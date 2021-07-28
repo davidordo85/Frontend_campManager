c8a9e6072b636922be24dd4d1ef23e740060b3c0
David Ordoñez <davidordonezescacena@hotmail.es>
2021-07-28 19:39:06 +0200

    detail boostrap

--- client/src/components/CampDetail/CampDetail.js
+++ client/src/components/CampDetail/CampDetail.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Layout from '../layout/layout';
-import { Button } from 'react-bootstrap';
 import { getCampDetail } from '../../api/camps';
 import Loader from '../Loader/Loader';
+import { Button, Card, Alert, ListGroup, CardColumns } from 'react-bootstrap';
 
 import './CampDetail.css';
 
 const CampDetail = ({ history, ...props }) => {
   const [camp, setCamp] = React.useState({
     activities: ['await'],
+    createdAt: '2021-07-28T00:18:07.898Z',
   });
   const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
@@ -34,88 +35,71 @@ const CampDetail = ({ history, ...props }) => {
     }
   };
 
+  const createdAt = camp.createdAt.split('T');
   console.log(camp);
 
   return (
     <Layout {...props}>
       <div className="detail">
         <Loader hidden={!loading} />
-        <h1 className={'title'}>Detail Camp</h1>
-        {/* TODO: que cambie la imagen de fondo como en la pagina inicial */}
-        <section className={'section'}>
-          <div className={'container-title'}>
-            <h2 className={'title-container'}>Name and direction</h2>
-            <div className={'name'}>
-              <p className={'title-description'}>Name camp: </p>
-              <p className={'camp'}>{camp.name}</p>
-            </div>
-            <div className={'name'}>
-              <p className={'title-description'}>Direction:</p>
-              <p className={'camp'}>{camp.address}</p>
-            </div>
-            <div className={'name'}>
-              <p className={'title-description'}>Location:</p>
-              <p className={'camp'}>{camp.location}</p>
-            </div>
-            <div className={'name'}>
-              <p className={'title-description'}>Place:</p>
-              <p className={'camp'}>{camp.tag}</p>
-            </div>
-            <div className={'name'}>
-              <p className={'title-description'}>Edition:</p>
-              <p className={'camp'}>{camp.edition}</p>
-            </div>
-          </div>
-          <div className={'container-title'}>
-            <h2 className={''}>Description</h2>
-            <div className={'name'}>
-              <p className={'title-description'}>Description:</p>
-              <p className={'camp'}>{camp.description}</p>
-            </div>
-            <div className={'name'}>
-              <p className={'title-description'}>Activities:</p>
-              {camp.activities.map((activity, index) => (
-                <li className={'activities'} key={index}>
-                  {activity}
-                </li>
-              ))}
-            </div>
-          </div>
-          <div className={'container-title'}>
-            <h2 className={''}>Dates</h2>
-            <div className={'name'}>
-              <p className={'title-description'}>Date creation:</p>
-              <time className={'camp'}>{camp.createdAt}</time>
-            </div>
-            <div className={'name'}>
-              <p className={'title-description'}>Date from:</p>
-              <time className={'camp'}>{camp.from}</time>
-            </div>
-            <div className={'name'}>
-              <p className={'title-description'}>Date to:</p>
-              <time className={'camp'}>{camp.to}</time>
-            </div>
-          </div>
-          <div className={'container-title'}>
-            <h2 className={''}>Contact</h2>
-            <p className={'title-description'}>Email:</p>
-            <p className={'camp'}>{camp.email}</p>
-            <p className={'title-description'}>Phone:</p>
-            <p className={'camp'}>{camp.phone}</p>
-            <p className={'title-description'}>Helpers:</p>
-            <p className={'camp'}>{camp.helpers}</p>
-            <p className={'title-description'}>Guests:</p>
-            <p className={'camp'}>{camp.guests}</p>
-          </div>
-          <Button size="lg" variant="success" className="sign">
-            Sign up
-          </Button>
-        </section>
-        {error && (
-          <div onClick={resetError} className="loginPage-error">
-            {error.message}
-          </div>
-        )}
+        <Card className="card-exterior">
+          <Card.Header className="detail-header">Detail Camp</Card.Header>
+          <Card.Text className="detail-text">{camp.name}</Card.Text>
+          <CardColumns className="columns">
+            <Card className="detail-description">
+              <Card.Header className="detail-header">Description</Card.Header>
+              <Card.Text> direction: {camp.address}</Card.Text>
+              <Card.Text> location: {camp.location}</Card.Text>
+              <Card.Text> edition: {camp.edition}</Card.Text>
+            </Card>
+            <Card>
+              <Card.Header className="detail-header">Detail</Card.Header>
+              <Card.Text>{camp.description}</Card.Text>
+              <ListGroup>
+                <Card.Header>activities:</Card.Header>
+                {camp.activities.map((activity, index) => (
+                  <ListGroup.Item className={'activities'} key={index}>
+                    {activity}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card>
+            <Card>
+              <Card.Header className="detail-header">Dates</Card.Header>
+              <Card.Text>
+                <span>created: </span>
+                <time>{createdAt[0]}</time>
+              </Card.Text>
+              <Card.Text>
+                <span>from: </span>
+                <time>{camp.from}</time>
+              </Card.Text>
+              <Card.Text>
+                <span>to: </span>
+                <time>{camp.to}</time>
+              </Card.Text>
+            </Card>
+            <Card>
+              <Card.Header className="detail-header">Contact</Card.Header>
+              <Card.Text>phone: {camp.phone}</Card.Text>
+              <Card.Text>email: {camp.email}</Card.Text>
+              <Card.Text>helpers: {camp.helpers}</Card.Text>
+              <Card.Text>guests: {camp.guests}</Card.Text>
+            </Card>
+            <Button variant="outline-dark" className="sign-up">
+              Sign up
+            </Button>
+          </CardColumns>
+          {error && (
+            <Alert
+              variant="danger"
+              onClick={resetError}
+              className="loginPage-error"
+            >
+              {error.message}
+            </Alert>
+          )}
+        </Card>
       </div>
     </Layout>
   );
