fcb8ab49054c63822a9764db270313e5620d5a72
David Ordoñez <davidordonezescacena@hotmail.es>
2021-08-16 14:10:10 +0200

    Modify camp form

--- src/components/admin/ModifyCamp/ModifyCampPage.js
+++ src/components/admin/ModifyCamp/ModifyCampPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../../layout/layout';
 import { getCampDetail } from '../../../api/camps';
-import { Alert } from 'react-bootstrap';
+import { Card, Form, Alert, Button } from 'react-bootstrap';
+import SelectTag from '../CreateCamp/SelectTag';
 import Loader from '../../Loader/Loader';
+import Select from 'react-select';
+import './ModifyCampPage.css';
 
 const ModifyCampPage = ({ history, ...props }) => {
-  const [camp, setCamp] = React.useState([]);
+  const [oldCamp, setOldCamp] = React.useState([]);
 
   const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
@@ -14,34 +17,206 @@ const ModifyCampPage = ({ history, ...props }) => {
   const paramsId = props.match.params.id;
 
   React.useEffect(() => {
-    campDetail(paramsId);
+    oldCampDetail(paramsId);
   }, [paramsId]);
 
-  const campDetail = async props => {
+  const oldCampDetail = async props => {
     setLoading(true);
     setError(null);
     try {
       const detailCamp = await getCampDetail(props);
-      setCamp(detailCamp.data);
+      setOldCamp(detailCamp.data);
     } catch (error) {
       setError(error);
     } finally {
       setLoading(false);
     }
   };
+
+  const options = [
+    { value: 'pool', label: 'pool' },
+    { value: 'reading', label: 'reading' },
+    { value: 'conference', label: 'conference' },
+    { value: 'crafts workshop', label: 'crafts workshop' },
+    { value: 'museum', label: 'museum' },
+    { value: 'meditation', label: 'meditation' },
+    { value: 'recycling workshop', label: 'recycling workshop' },
+    { value: 'seminar', label: 'seminar' },
+    { value: 'show', label: 'show' },
+  ];
+
+  const [act, setAct] = React.useState([]);
+
+  console.log(oldCamp.activities);
+
+  const handleActivities = selectedOptions => {
+    setAct([].slice.call(selectedOptions).map(item => item.value));
+  };
+
   return (
     <Layout {...props}>
       <Loader hidden={!loading} />
-      <div>{JSON.stringify(camp)}</div>
-      {error && (
-        <Alert
-          variant="danger"
-          onClick={resetError}
-          className="loginPage-error"
-        >
-          {error.message}
-        </Alert>
-      )}
+      <Card className="card-form">
+        <Form className="form-modify">
+          <Form.Group className="form-groupModify">
+            <div>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                className=""
+                type="text"
+                name="name"
+                placeholder={oldCamp.name}
+                //value={name}
+                //onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <div>
+              <Form.Label>Edition</Form.Label>
+              <Form.Control
+                className=""
+                type="text"
+                name="edition"
+                placeholder={oldCamp.edition}
+                //value={edition}
+                //onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <div>
+              <Form.Label>Location</Form.Label>
+              <Form.Control
+                type="text"
+                name="location"
+                className=""
+                placeholder={oldCamp.location}
+                //value={location}
+                //onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <div>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                name="description"
+                className=""
+                placeholder={oldCamp.description}
+                //value={description}
+                //onChange={handleCreateCamp}
+              />
+            </div>
+            <div>
+              <SelectTag
+                className=""
+                name="tag"
+                placeholder={oldCamp.tag}
+                //value={tag}
+                //handleChange={handleCreateCamp}
+              />
+            </div>
+            <div>
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                name="address"
+                className=""
+                placeholder={oldCamp.address}
+                //value={address}
+                //onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <div className="activities">
+              <Form.Label className="">Activities</Form.Label>
+              <Select
+                className="activities"
+                classNamePrefix="activities"
+                isMulti
+                isClearable={false}
+                options={options}
+                onChange={handleActivities}
+              />
+            </div>
+            <div>
+              <Form.Label>Phone</Form.Label>
+              <Form.Control
+                type="text"
+                name="phone"
+                className=""
+                placeholder={oldCamp.phone}
+                //value={phone}
+                //onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <div>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                className=""
+                placeholder={oldCamp.email}
+                //value={email}
+                //onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <div>
+              <Form.Label>From</Form.Label>
+              <Form.Control
+                className=""
+                type="date"
+                name="from"
+                placeholder={oldCamp.from}
+                //value={from}
+                //onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <div>
+              <Form.Label>To</Form.Label>
+              <Form.Control
+                className=""
+                type="date"
+                name="to"
+                placeholder={oldCamp.to}
+                //value={to}
+                // onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <div>
+              <Form.Label>Capacity</Form.Label>
+              <Form.Control
+                type="number"
+                name="capacity"
+                className=""
+                placeholder={oldCamp.capacity}
+                //value={capacity}
+                //onChange={handleCreateCamp}
+                required
+              />
+            </div>
+            <Button
+              variant="outline-dark"
+              type="submit"
+              className="button-createCamp"
+            >
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+        {error && (
+          <Alert
+            variant="danger"
+            onClick={resetError}
+            className="loginPage-error"
+          >
+            {error.message}
+          </Alert>
+        )}
+      </Card>
     </Layout>
   );
 };
