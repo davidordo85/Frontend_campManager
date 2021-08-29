173c9a3d24247d2877b1596205b2e8023a2de911
David Ordoñez <davidordonezescacena@hotmail.es>
2021-08-29 16:28:15 +0200

    cancel fix

--- src/components/index/CampList.js
+++ src/components/index/CampList.js
@@ -47,7 +47,6 @@ const CampList = ({ id, history, location, ...props }) => {
       setLoading(false);
     }
   };
-  //prueba
   return (
     <Layout {...props}>
       <div className="con-camps">
